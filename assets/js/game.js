88288a982d0eae19c8f14d52ee22a1cc50c4e990
AlexRasch <46262688+AlexRasch@users.noreply.github.com>
2023-05-08 15:59:49 +0200

    added spirits

---
+++ assets/js/game.js
@@ -0,0 +1,53 @@
+kaboom({
+    global: true,
+    fullscreen: true,
+    scale: 1,
+    debug: true,
+    clearColor: [0, 0, 0, 1],
+})
+
+loadRoot('assets/img/')
+loadSprite('brick', 'brick.png')
+loadSprite('block', 'block.png')
+loadSprite('coin', 'coin.png')
+loadSprite('evil-shroom', 'evil-shroom.png')
+loadSprite('mario', 'mario.png')
+loadSprite('mushroom', 'mushroom.png')
+loadSprite('surprise', 'surprise.png')
+loadSprite('unboxed', 'unboxed.png')
+// Pipe
+loadSprite('pipe-top-left', 'pipe-top-left.png')
+loadSprite('pipe-top-right', 'pipe-top-right.png')
+loadSprite('pipe-bottom-left', 'pipe-bottom-left.png')
+loadSprite('pipe-bottom-right', 'pipe-bottom-right.png')
+
+scene("game", () => {
+    layers(['bg', 'obj', 'ui'], 'obj')
+
+    const map = [
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '                                                    ',
+        '====================================================',
+    ]
+
+    const levelCfg = {
+        width: 20,
+        height: 20,
+        '=': [sprite('block', solid)]
+    }
+
+    const gameLevel = addLevel(map,levelCfg)
+
+})
+
+
+start("game")
